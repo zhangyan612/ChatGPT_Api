e509670bb2410f891a8ed5486732012f0a7a33d6
Yan Zhang <zhangyan612@gmail.com>
2023-10-09 01:34:33 -0500

    ability for code dependency building

---
+++ llama/interprete.py
@@ -0,0 +1,6 @@
+import interpreter
+
+interpreter.model = "gpt-3.5-turbo"
+
+interpreter.chat("Plot AAPL and META's normalized stock prices") # Executes a single command
+interpreter.chat() # Starts an interactive chat
--- tools/functionVisit.py
+++ tools/functionVisit.py
@@ -1,42 +1,46 @@
 import ast
 
 class FunctionVisitor(ast.NodeVisitor):
-    def __init__(self):
+    def __init__(self, filename):
         self.current_function = None
-        self.function_deps = {}
+        self.function_deps = {filename: {}}
+        self.filename = filename
 
     def visit_FunctionDef(self, node):
         self.current_function = node.name
-        self.function_deps[self.current_function] = set()
+        self.function_deps[self.filename][self.current_function] = set()
         self.generic_visit(node)
         self.current_function = None
 
+    def visit_ClassDef(self, node):
+        for subnode in node.body:
+            if isinstance(subnode, ast.FunctionDef):
+                self.visit_FunctionDef(subnode)
+
     def visit_Call(self, node):
-        if isinstance(node.func, ast.Name):
+        if isinstance(node.func, ast.Attribute) and isinstance(node.func.value, ast.Name) and node.func.value.id == 'self':
             if self.current_function:
-                self.function_deps[self.current_function].add(node.func.id)
+                self.function_deps[self.filename][self.current_function].add(node.func.attr)
         self.generic_visit(node)
 
-def create_dependency_tree(source_code):
+def create_dependency_tree(source_code, filename):
     tree = ast.parse(source_code)
-    visitor = FunctionVisitor()
+    visitor = FunctionVisitor(filename)
     visitor.visit(tree)
     return visitor.function_deps
 
 # Test with some code
 code = """
-# this is comment of a
-def a():
-    if 1 == 1:
-        c()
-    b()
-    b()
+class MyClass:
+    def method_a(self):
+        self.method_b()
+        self.method_c()
 
-def b():
-    c()
+    def method_b(self):
+        self.method_c()
 
-def c():
-    print('nothing to call')
+    def method_c(self):
+        pass
 """
 
-print(create_dependency_tree(code))
+print(create_dependency_tree(code, 'test.py'))
