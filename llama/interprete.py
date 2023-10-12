5e3a349124225244802b8184e57743c432a02ee2
Yan Zhang <zhangyan612@gmail.com>
2023-10-12 00:41:21 -0500

    Add running instruction

--- llama/interprete.py
+++ llama/interprete.py
@@ -4,3 +4,7 @@ interpreter.model = "gpt-3.5-turbo"
 
 interpreter.chat("Plot AAPL and META's normalized stock prices") # Executes a single command
 interpreter.chat() # Starts an interactive chat
+
+# how to run 
+# conda activate huggingface
+# interpreter -y
