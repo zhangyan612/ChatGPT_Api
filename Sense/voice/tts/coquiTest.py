9b899d626f86daa94298543f52ae9da1401df46c
Yan Zhang <zhangyan612@gmail.com>
2023-11-10 01:00:03 -0600

    add tts

---
+++ Sense/voice/tts/coquiTest.py
@@ -0,0 +1,16 @@
+import torch
+from TTS.api import TTS
+
+# Get device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(device)
+# List available TTS models
+print(TTS().list_models())
+
+# Init TTS
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+
+# Run TTS
+# ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
+# Text to speech list of amplitude values as output
+wav = tts.tts(text="Hello world!", speaker_wav="my/cloning/audio.wav", language="en")
