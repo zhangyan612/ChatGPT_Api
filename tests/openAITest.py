ee08b154a1050a58dd6985d91be5abe454f8f8fe
Yan Zhang <zhangyan612@gmail.com>
2024-01-05 01:05:22 -0600

    multi use case include streaming

--- tests/openAITest.py
+++ tests/openAITest.py
@@ -3,12 +3,40 @@ from openai import OpenAI
 
 
 client = OpenAI()
-completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-        {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-    ]
+
+
+# completion = client.chat.completions.create(
+#     model="gpt-3.5-turbo",
+#     messages=[
+#         {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+#         {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+#     ]
+# )
+
+# print(completion.choices[0].message)
+
+# JSON mode
+# response = client.chat.completions.create(
+#   model="gpt-3.5-turbo-1106",
+#   response_format={ "type": "json_object" },
+#   messages=[
+#     {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
+#     {"role": "user", "content": "Who won the world series in 2020?"}
+#   ]
+# )
+
+# print(response)
+# print(response.choices[0].message.content)
+
+
+# streaming
+stream = client.chat.completions.create(
+    model="gpt-4",
+    messages=[{"role": "user", "content": "write a function in python to read files from local disk"}],
+    stream=True,
 )
 
-print(completion.choices[0].message)
+for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end="")
+
